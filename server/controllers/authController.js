e159a2c59254959a528a0092ccafb9d7b70ae3a3
Nicholas D'Amato <Nicholasvdamato@gmail.com>
2021-05-20 12:05:25 -0600

    first commit

---
+++ server/controllers/authController.js
@@ -0,0 +1,44 @@
+const bcrypt = require('bcryptjs')
+
+module.exports = {
+    register: async (req, res) => {
+        const db = req.app.get('db')
+        const { email, password } = req.body
+        const [result] = await db.auth.check_email(email)
+        if (result) {
+            return res.status(409).send('Email is already taken')
+        }
+        const salt = bcrypt.genSaltSync(10)
+        const hash = bcrypt.hashSync(password, salt)
+        const [user] = await db.auth.register_user(email, hash)
+        const [cart] = await db.cart.create_cart(user.user_id)
+        delete user.password
+        req.session.user = user
+        req.session.user.card_id = cart.card_id
+        return res.status(200).send(req.session.user)
+    },
+    login: async (req, res) => {
+        const db = req.app.get('db')
+        const { email, password } = req.body
+        const [user] = await db.auth.check_email(email)
+        if (!user) {
+            return res.status(401).send('User is not registered')
+        }
+        const isAuthenticated = bcrypt.compareSync(password, user.password)
+        if (!isAuthenticated) {
+            return res.status(401).send('Password is incorrect')
+        }
+        const [cart] = await db.cart.get_cart(user.user_id)
+        delete user.password
+        req.session.user = user
+        req.session.user.cart_id = cart.card_id
+        return res.status(200).send(req.session.user)
+    },
+    logout: (req, res) => {
+        req.session.destroy()
+        res.sendStatus(200)
+    },
+    getUser: (req, res) => {
+
+    }
+}
--- server/index.js
+++ server/index.js
@@ -10,10 +10,12 @@ const express = require('express')
 const massive = require('massive')
 const session = require('express-session')
 
-const {CONNECTION_STRING, SESSION_SECRET, SERVER_PORT} = process.env
+const { CONNECTION_STRING, SESSION_SECRET, SERVER_PORT } = process.env
 
 // CONTROLLERS
-
+const authCtrl = require('./controllers/authController')
+const productCtrl = require('./controllers/productController')
+const cartCtrl = require('./controllers/cartController')
 
 // APP INSTANCE CREATED
 const app = express()
@@ -24,19 +26,27 @@ app.use(session({
   secret: SESSION_SECRET,
   resave: false,
   saveUninitialized: true,
-  cookie: {maxAge: 1000 *60 *60 *24}
+  cookie: { maxAge: 1000 * 60 * 60 * 24 }
 }))
 
 // DATABASE CONNECTION
 massive({
   connectionString: CONNECTION_STRING,
-  ssl: {rejectUnauthorized: false}
+  ssl: { rejectUnauthorized: false }
 })
-.then(db =>{
-  app.set('db', db)
-  console.log("Database Connected")
-  app.listen(SERVER_PORT, () => console.log(`Server listening on ${SERVER_PORT}`))
-}).catch(err => console.log(err))
+  .then(db => {
+    app.set('db', db)
+    console.log("Database Connected")
+    app.listen(SERVER_PORT, () => console.log(`Server listening on ${SERVER_PORT}`))
+  }).catch(err => console.log(err))
 
 
 // ENDPOINTS
+app.post('/auth/register', authCtrl.register)
+app.post('/auth/login', authCtrl.login)
+app.get('/auth/logout', authCtrl.logout)
+
+app.get('/api/products', productCtrl.getProducts)
+
+app.get('/api/cart', cartCtrl.getCart)
+app.post(`/api/cart/:product_id`, cartCtrl.addToCart)
